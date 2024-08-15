eee6056e12f8a7ab6f079e823b8782ff0e5de2ac
Poorab Kumar <poorabkumarashu@gmail.com>
2024-08-15 19:45:46 +0530

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -52,6 +52,11 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+// Root route for testing
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
